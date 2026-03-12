9ddbe94895e9b191b7d3670d18bd7388988d5370
Sridhar Ratnakumar <srid@srid.ca>
2026-03-12 17:10:38 -0400

    fix: home-manager module uses default opencode, wrapper only for nix run

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,11 +1,7 @@
 { config, lib, pkgs, ... }:
-let
-  opencodeJuspay = import ./juspay/package.nix { inherit pkgs lib; };
-in
 {
   programs.opencode = {
     enable = true;
-    package = opencodeJuspay;
     settings = import ./juspay/settings.nix;
   };
 }
